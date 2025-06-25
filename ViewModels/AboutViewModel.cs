759b6de1022adc50efe7b69fefbc93d704ce824e
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-06-25 12:10:12 -0500

    Se implementa el about como mvvm

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
 
 namespace Grupo1NotasMVVM.ViewModels
 {
     internal class AboutViewModel
     {
+        public string Nombre => "Luis Elian Pineda ";
+        public string Descripcion => "Un about regenerico";
+
+        public string Icono => "pinguino.png";
+
+ 
     }
 }
