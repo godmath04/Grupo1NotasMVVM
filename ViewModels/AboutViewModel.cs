26632bdcb36b233dd7b167f2f8dd269a1b248704
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-06-25 13:08:34 -0500

    Se implementa el modelo MVVM en en todo el proyecto de notas

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -10,11 +10,25 @@ namespace Grupo1NotasMVVM.ViewModels
 {
     internal class AboutViewModel
     {
-        public string Nombre => "Luis Elian Pineda ";
-        public string Descripcion => "Un about regenerico";
+        //public string Nombre => "Luis Elian Pineda ";
+        //public string Descripcion => "Un about regenerico";
 
-        public string Icono => "pinguino.png";
+        //public string Icono => "pinguino.png";
 
- 
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public ICommand ShowMoreInfoCommand { get; }
+
+        public AboutViewModel()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+        async Task ShowMoreInfo() =>
+            await Launcher.Default.OpenAsync(MoreInfoUrl);
+
+
     }
 }
