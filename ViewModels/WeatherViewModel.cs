09b604f44a1f558d87aefd81bb6dfb6aaa9e1ad6
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-06-25 14:10:55 -0500

    chor: minor changes

--- ViewModels/WeatherViewModel.cs
+++ ViewModels/WeatherViewModel.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Grupo1NotasMVVM.Repositories;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using static Grupo1NotasMVVM.Models.WeatherModel;
 
 namespace Grupo1NotasMVVM.ViewModels
 {
-    internal class WeatherViewModel
+    internal class WeatherViewModel : INotifyPropertyChanged
     {
+        private WeatherData _weatherData;
+
+        public WeatherData WeatherDataInfo
+        {
+            get => _weatherData;
+            set
+            {
+                if (_weatherData != value)
+                {
+                    _weatherData = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        public WeatherViewModel()
+        {
+            _weatherData = new WeatherData();
+            GetCurrentWeather();
+
+
+        }
+        //Para conectarme al repositoritorio
+        public async Task GetCurrentWeather()
+        {
+            WeatherRepository weatherRepository = new WeatherRepository();
+            WeatherDataInfo = await weatherRepository.GetWeatherCurrentLocationAsync();
+
+        }
+
+
+        /*-------------------------------------------------------------------*/
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
