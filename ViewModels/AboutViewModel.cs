f2d0fe0f096533526923d6df5042c2b9eec047a7
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-06-25 15:36:19 -0500

    Se implementan los cambios en el about

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Input;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Grupo1NotasMVVM.Models;
 
 namespace Grupo1NotasMVVM.ViewModels
 {
@@ -14,20 +16,22 @@ namespace Grupo1NotasMVVM.ViewModels
         //public string Descripcion => "Un about regenerico";
 
         //public string Icono => "pinguino.png";
-
-        public string Title => AppInfo.Name;
-        public string Version => AppInfo.VersionString;
-        public string MoreInfoUrl => "https://aka.ms/maui";
-        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public ObservableCollection<About> Miembros { get; }
         public ICommand ShowMoreInfoCommand { get; }
 
         public AboutViewModel()
         {
             ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+
+            Miembros = new ObservableCollection<About>
+            {
+                new About { Nombre = "Luis Elian Pineda", Edad = 21, DeporteFavorito = "Ajedrez", Foto = "pinguino.png" },
+                new About { Nombre = "Galo Guevara", Edad = 22, DeporteFavorito = "Futbol", Foto = "galo1.jpg" }
+            };
         }
 
         async Task ShowMoreInfo() =>
-            await Launcher.Default.OpenAsync(MoreInfoUrl);
+            await Launcher.Default.OpenAsync("https://aka.ms/maui");
 
 
     }
