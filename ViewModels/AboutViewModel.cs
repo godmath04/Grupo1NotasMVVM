4a3efb7ee7c13f036c5e79d491c9d9e2a6719849
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-06-25 16:18:49 -0500

    Version 1.0.0

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using Grupo1NotasMVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.Input;
 using System.Windows.Input;
-using System.Collections.ObjectModel;
-using Grupo1NotasMVVM.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Grupo1NotasMVVM.ViewModels
 {
@@ -25,8 +26,10 @@ namespace Grupo1NotasMVVM.ViewModels
 
             Miembros = new ObservableCollection<About>
             {
-                new About { Nombre = "Luis Elian Pineda", Edad = 21, DeporteFavorito = "Ajedrez", Foto = "pinguino.png" },
-                new About { Nombre = "Galo Guevara", Edad = 22, DeporteFavorito = "Futbol", Foto = "galo1.jpg" }
+                new About { Nombre = "Luis Elian Pineda", Edad = 24, DeporteFavorito = "Ajedrez", Foto = "pinguino.png" },
+                new About { Nombre = "Galo Guevara", Edad = 24, DeporteFavorito = "Futbol", Foto = "galo1.jpg" },
+                new About { Nombre = "Javier Arias", Edad = 22, DeporteFavorito = "Futbol", Foto = "javi1.jpg" },
+                new About { Nombre = "Julian Torres", Edad = 22, DeporteFavorito = "Basquet", Foto = "julian1.png" }
             };
         }
 
